cdfc20d730c7a3d9221e94425d5296c62b71192a
geonhui54 <dududi042811@naver.com>
2021-07-23 05:33:50 +0900

    Update

---
+++ models/UserStorage.js
@@ -0,0 +1,33 @@
+
+const fs = require("fs").promises;
+
+class UserStorage {
+    static getUsers(...fields) {
+        // const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) =>{
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+    
+    static getUserInfo(id){
+        return fs.readFile("./databases/users.json")
+            .then((data) => {
+                const users = JSON.parse(data);
+                const idx = users.id.indexOf(id);
+                const userKeys =Object.keys(users);
+                const userInfo = userKeys.reduce((newUser, info) => {
+                    newUser[info] = users[info][idx];
+                    return newUser;
+                }, {});
+                
+                return userInfo; 
+            })
+            .catch(console.error);
+    }
+}
+
+module.exports = UserStorage;
